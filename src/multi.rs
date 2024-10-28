9e88cc4bc16aab414af7654371987d1720dbd6bb
Emmanuel Briot <briot.emmanuel@gmail.com>
2024-10-28 16:42:21 +0100

    (multi): add a lot of new functions

--- src/multi.rs
+++ src/multi.rs
@@ -1,6 +1,7 @@
 use crate::intervals::Interval;
 use crate::leftmostiter::LeftMostIter;
 use crate::nothing_between::NothingBetween;
+use crate::pairs::Pair;
 
 /// A sorted list of non-overlapping intervals.
 ///
@@ -20,6 +21,91 @@ use crate::nothing_between::NothingBetween;
 pub struct MultiInterval<T>(Vec<Interval<T>>);
 
 impl<T> MultiInterval<T> {
+    /// Returns an empty multi interval
+    /// ```none
+    ///    {}
+    /// ```
+    pub fn empty() -> Self {
+        MultiInterval(Vec::new())
+    }
+
+    /// Create a multi-interval that contains a single value
+    /// ```none
+    ///    { [value, value] }
+    /// ```
+    pub fn new_single(value: T) -> Self
+    where
+        T: Clone,
+    {
+        MultiInterval(vec![Interval::new_single(value)])
+    }
+
+    /// Create a multi-interval from a collection of intervals.
+    /// Those intervals do not have to be sorted, or non-overlapping
+    pub fn new<I>(iter: I) -> Self
+    where
+        T: Ord + NothingBetween + Clone,
+        I: IntoIterator<Item = Interval<T>>,
+    {
+        let mut m = MultiInterval::empty();
+        m.extend(iter);
+        m
+    }
+
+    /// Create a multi-interval from a pair (returned by
+    /// `Interval::difference()` for instance).
+    /// It is assumed that the intervals in pair do not overlap and are
+    /// sorted, as is the case when they are returned by difference().
+    pub fn from_pair(pair: Pair<T>) -> Self
+    where
+        T : PartialOrd + NothingBetween,
+    {
+        match pair {
+            Pair::One(intv) => MultiInterval(vec![intv]),
+            Pair::Two(intv1, intv2) => MultiInterval(vec![intv1, intv2]),
+        }
+    }
+
+    /// Return the lowest valid value amongst all the intervals, or None if
+    /// self is empty or if the left-most interval is unbounded.
+    /// This value might not actually be valid for self, if we have an
+    /// open bound for instance.
+    pub fn lower(&self) -> Option<&T> {
+        match self.0.first() {
+            None => None,
+            Some(f) => f.lower(),
+        }
+    }
+
+    /// True if the left-most interval is unbounded.
+    /// This is false if self is empty.
+    pub fn lower_unbounded(&self) -> bool {
+        match self.0.first() {
+            None => false,
+            Some(f) => f.lower_unbounded(),
+        }
+    }
+
+    /// Return the highest valid value amongst all the intervals, or None if
+    /// self is empty or if the right-most interval is unbounded.
+    /// This value might not actually be valid for self, if we have an
+    /// open bound for instance.
+    pub fn upper(&self) -> Option<&T> {
+        match self.0.last() {
+            None => None,
+            Some(f) => f.upper(),
+        }
+    }
+
+    /// True if the right-most interval is unbounded.
+    /// This is false if self is empty.
+    pub fn upper_unbounded(&self) -> bool {
+        match self.0.last() {
+            None => false,
+            Some(f) => f.upper_unbounded(),
+        }
+    }
+
     /// Return the number of intervals in self.
     pub fn len(&self) -> usize {
         self.0.len()
@@ -84,9 +170,10 @@ impl<T> MultiInterval<T> {
         // Special case: we are inserting at the end of self.  No need to
         // create a new vector.
         let last = self.0.last();
-        if last.is_none()   // self is empty
-           || last.unwrap().strictly_left_not_contiguous(
-                elements.first().unwrap())
+        if last.is_none()
+            || last
+                .unwrap()
+                .strictly_left_not_contiguous(elements.first().unwrap())
         {
             self.extend_internal(elements);
         } else {
@@ -102,6 +189,45 @@ impl<T> MultiInterval<T> {
     /// Iterate over all intervals
     pub fn iter(&self) -> impl Iterator<Item = &Interval<T>> {
         self.0.iter()
+    }
+
+    /// Whether value is valid for any of the intervals in self
+    pub fn contains<V>(&self, value: V) -> bool
+    where
+        T: PartialOrd + NothingBetween,
+        V: ::core::borrow::Borrow<T>,
+    {
+        let t = value.borrow();
+        self.0.iter().any(|v| v.contains(t))
+    }
+
+    /// Whether all values in other are valid for self
+    pub fn contains_interval<U>(&self, other: U) -> bool
+    where
+        T: PartialOrd + NothingBetween,
+        U: ::core::borrow::Borrow<Interval<T>>,
+    {
+        // In the of joining, other must be fully contained in one of the
+        // nested intervals.
+        let u = other.borrow();
+        self.0.iter().any(|v| v.contains_interval(u))
+    }
+
+    /// Returns the convex hull, i.e. the smallest intervals that contains
+    /// all values in all intervals in self.  The result might contain
+    /// additional values that were not valid for self.
+    pub fn convex_hull(&self) -> Interval<T>
+    where
+        T: PartialOrd + NothingBetween + Clone
+    {
+        if self.0.is_empty() {
+            Interval::empty()
+        } else {
+            Interval {
+                lower: self.0.first().unwrap().lower.clone(),
+                upper: self.0.last().unwrap().upper.clone(),
+            }
+        }
     }
 
     /// Check that self is well-formed:
@@ -172,7 +298,7 @@ mod tests {
         //      +                 [4 5)
         //      = {[1                5)}
 
-        let mut m = MultiInterval::default();
+        let mut m = MultiInterval::empty();
         insert_via_extend(
             &mut m,
             [interval!(1, 3), interval!(2, 4), interval!(4, 5)],
@@ -253,9 +379,82 @@ mod tests {
         assert!(m.is_empty());
         assert_eq!(m.len(), 0);
         assert_eq!(m, empty);
+        assert!(!m.lower_unbounded());
+        assert!(!m.upper_unbounded());
+        assert_eq!(m.lower(), None);
+        assert_eq!(m.upper(), None);
+        assert_eq!(m.convex_hull(), Interval::empty());
 
         m.add(Interval::empty());
         m.add(Interval::empty());
         assert!(m.is_empty());
+    }
+
+    #[test]
+    fn test_contains() {
+        let m = MultiInterval::new_single(4);
+        assert!(m.contains(4));
+        assert!(!m.contains(5));
+        assert!(!m.contains(3));
+        assert_eq!(m.lower(), Some(&4));
+        assert_eq!(m.upper(), Some(&4));
+        assert!(!m.lower_unbounded());
+        assert!(!m.upper_unbounded());
+        assert_eq!(m.convex_hull(), interval!(4, 4, "[]"));
+
+        let m = MultiInterval::new([interval!(1, 3), interval!(5, 7)]);
+        assert!(m.contains(1));
+        assert!(m.contains(2));
+        assert!(!m.contains(3));
+        assert!(!m.contains(4));
+        assert!(m.contains(5));
+        assert!(m.contains(6));
+        assert!(!m.contains(7));
+        assert!(m.contains_interval(interval!(1, 2)));
+
+        #[allow(clippy::needless_borrows_for_generic_args)]
+        {
+           assert!(m.contains_interval(&interval!(6, 7)));   //  accepts ref
+        }
+        assert!(!m.contains_interval(interval!(3, 7)));
+        assert_eq!(m.lower(), Some(&1));
+        assert_eq!(m.upper(), Some(&7));
+        assert!(!m.lower_unbounded());
+        assert!(!m.upper_unbounded());
+        assert_eq!(m.convex_hull(), interval!(1, 7));
+    }
+
+    #[test]
+    fn test_unbounded() {
+        let m = MultiInterval::new([interval!(1, "inf")]);
+        assert!(!m.lower_unbounded());
+        assert!(m.upper_unbounded());
+        assert_eq!(m.lower(), Some(&1));
+        assert_eq!(m.upper(), None);
+
+        let m = MultiInterval::new([interval!("-inf", 1, "]")]);
+        assert!(m.lower_unbounded());
+        assert!(!m.upper_unbounded());
+        assert_eq!(m.lower(), None);
+        assert_eq!(m.upper(), Some(&1));
+    }
+
+    #[test]
+    fn test_equals() {
+        let m1 = MultiInterval::new([
+            interval!(3, 10, "[]"),
+            interval!(15, 20, "()"),
+        ]);
+        let m2 = MultiInterval::new([
+            interval!(2, 5, "()"),
+            interval!(5, 11, "[)"),
+            interval!(16, 19, "[]"),
+        ]);
+        assert_eq!(m1, m2);
+
+        let intv1 = interval!(3, 20, "[)");
+        let pairs = intv1 - interval!(10, 15, "(]");
+        let m3 = MultiInterval::from_pair(pairs);
+        assert_eq!(m1, m3);
     }
 }
